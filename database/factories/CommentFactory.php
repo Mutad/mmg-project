dc3897f030d43e9b46ae532d545214196deaacac
mutado <mutado.nzr@gmail.com>
2020-07-13 18:05:33 +0300

    Fixed bug when category or comment dont exist

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -11,6 +11,16 @@ $factory->define(Comment::class, function (Faker $faker) {
         App\Post::class
     ];
 
+    // Creating at least one category and post for comment
+    if (App\Category::all()->count() == 0) {
+        factory('App\Category')->create();
+    }
+    if (App\Post::all()->count() == 0) {
+        factory('App\Post')->create();
+    }
+
+    //Selecting random commentable type from array
+    //and selecting random id of this type
     $commentableType = $faker->randomElement($commentable);
     $commentableId = $commentableType::all(['id'])->random();
 
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -6,6 +6,12 @@ use App\Post;
 use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
+
+    //Creating at least one category for post seeding
+    if (App\Category::all()->count() == 0){
+        factory('App\Category')->create();
+    }
+
     return [
         'name'=>$faker->sentence,
         'content'=>$faker->realtext(200,2),
